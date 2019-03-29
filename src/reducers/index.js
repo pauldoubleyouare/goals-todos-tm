1b92ac29865bf7f5dc073e6a3e17c73ba202482e
Paul Rosenberg <paulrosenberg13@gmail.com>
2019-03-28 18:52:27 -0700

    second

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// export all of the reducers
+import { combineReducers } from 'redux'
+
+import todos from './todos'
+import goals from './goals'
+import loading from './loading'
+
+export default combineReducers({
+  todos,
+  goals,
+  loading
+})
